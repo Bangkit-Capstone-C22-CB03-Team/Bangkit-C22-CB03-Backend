debbf6f68df0dfe4d61dbd5eb78e893a7f6a3447
Aclaputra <muhamadacla22@gmail.com>
2022-05-20 04:13:05 +0700

    remove debug=true

--- CC/app/src/app.py
+++ CC/app/src/app.py
@@ -25,4 +25,4 @@ class TestPredict(Resource):
 api.add_resource(TestPredict, '/')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080)
