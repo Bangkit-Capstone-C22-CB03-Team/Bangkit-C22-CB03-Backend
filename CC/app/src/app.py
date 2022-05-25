06851a2c634fed5b193bcaa7f3568454418f33d1
ArielP.G <kouci005@gmail.com>
2022-05-25 14:31:29 +0700

    Update app.py
    update fix os.environ PORT

--- CC/app/src/app.py
+++ CC/app/src/app.py
@@ -29,4 +29,4 @@ class TestPredict(Resource):
 api.add_resource(TestPredict, '/')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 8080)))
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 8080)))
