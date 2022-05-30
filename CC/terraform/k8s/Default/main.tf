b7234cb79aa62da822830614eb86bad92fcf65c7
Aclaputra <muhamadacla22@gmail.com>
2022-05-31 01:01:39 +0700

    move default terraform k8s config to Default folder

---
+++ CC/terraform/k8s/Default/main.tf
@@ -0,0 +1,86 @@
+variable "region" {
+  default = "us-central1"
+}
+
+variable "location" {
+  default = "us-central1-b"
+}
+
+variable "network_name" {
+  default = "chatbot-traveloka"
+}
+
+provider "google" {
+  region = var.region
+}
+
+resource "google_compute_network" "default" {
+  project                 = "sacred-armor-346113"
+  name                    = var.network_name
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "default" {
+  project                  = "sacred-armor-346113"
+  name                     = var.network_name
+  ip_cidr_range            = "10.127.0.0/20"
+  network                  = google_compute_network.default.self_link
+  region                   = var.region
+  private_ip_google_access = true
+}
+
+data "google_client_config" "current" {
+}
+
+data "google_container_engine_versions" "default" {
+  project = "sacred-armor-346113"
+  location = var.location
+}
+
+resource "google_container_cluster" "default" {
+  project            = "sacred-armor-346113"
+  name               = var.network_name
+  location           = var.location
+  initial_node_count = 3
+  min_master_version = data.google_container_engine_versions.default.latest_master_version
+  network            = google_compute_subnetwork.default.name
+  subnetwork         = google_compute_subnetwork.default.name
+
+  // Use legacy ABAC until these issues are resolved:
+  //   https://github.com/mcuadros/terraform-provider-helm/issues/56
+  //   https://github.com/terraform-providers/terraform-provider-kubernetes/pull/73
+  enable_legacy_abac = true
+
+  # change machine type
+  node_config {
+    # gedein lagi machine typenya?
+    machine_type = "e2-standard-2"
+  }
+
+  // Wait for the GCE LB controller to cleanup the resources.
+  // Wait for the GCE LB controller to cleanup the resources.
+  provisioner "local-exec" {
+    when    = destroy
+    command = "sleep 90"
+  }
+}
+
+output "network" {
+  value = google_compute_subnetwork.default.network
+}
+
+output "subnetwork_name" {
+  value = google_compute_subnetwork.default.name
+}
+
+output "cluster_name" {
+  value = google_container_cluster.default.name
+}
+
+output "cluster_region" {
+  value = var.region
+}
+
+output "cluster_location" {
+  value = google_container_cluster.default.location
+}
