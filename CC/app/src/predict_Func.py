9ee99412e9b6c5e08d95f5fb8e534055817e0cf7
ArielP.G <kouci005@gmail.com>
2022-06-02 19:43:37 +0700

    Add modification for Predict Function module
    Add access to load model from Cloud Storage for backup and the main load model from our ML team Hugging face.

--- CC/app/src/predict_Func.py
+++ CC/app/src/predict_Func.py
@@ -1,16 +1,17 @@
 from transformers import TFAutoModelForQuestionAnswering, AutoTokenizer
 from transformers import pipeline
 
+# Load from our deployed model in Huggingface.co
 tokenizer = AutoTokenizer.from_pretrained(
-    "bert-large-uncased-whole-word-masking-finetuned-squad")
+    "Andaf/bert-uncased-finetuned-squad-indonesian")
 model = TFAutoModelForQuestionAnswering.from_pretrained(
-    "./bert_model/", return_dict=False)
-    
+    "Andaf/bert-uncased-finetuned-squad-indonesian", return_dict=False)
+
 nlp = pipeline('question-answering', model=model, tokenizer=tokenizer)
 
 def predict_func(question):
 
-    context = "The Amazon rainforest (Portuguese: Floresta Amazônica or Amazônia; Spanish: Selva Amazónica, Amazonía or usually Amazonia; French: Forêt amazonienne; Dutch: Amazoneregenwoud), also known in English as Amazonia or the Amazon Jungle, is a moist broadleaf forest that covers most of the Amazon basin of South America. This basin encompasses 7,000,000 square kilometres (2,700,000 sq mi), of which 5,500,000 square kilometres (2,100,000 sq mi) are covered by the rainforest. This region includes territory belonging to nine nations. The majority of the forest is contained within Brazil, with 60% of the rainforest, followed by Peru with 13%, Colombia with 10%, and with minor amounts in Venezuela, Ecuador, Bolivia, Guyana, Suriname and French Guiana. States or departments in four nations contain ""Amazonas"" in their names. The Amazon represents over half of the planet's remaining rainforests, and comprises the largest and most biodiverse tract of tropical rainforest in the world, with an estimated 390 billion individual trees divided into 16,000 species."
+    context = "Makanan kesukaan saya adalah rendang."
 
     result = nlp(
         question=question,
---
+++ CC/app/src/predict_Func2.py
@@ -0,0 +1,49 @@
+import os
+from transformers import TFAutoModelForQuestionAnswering, AutoTokenizer
+from transformers import pipeline
+from google.cloud import storage
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = './service-acc/qwiklabs-gcp-02-96ccf592dc5f-c92d453542da.json'
+storage_client = storage.Client()
+
+isExist = os.path.exists('./bert_model/')
+
+if not isExist:
+  # Create a new directory because it does not exist 
+  os.makedirs('./bert_model/')
+
+def donwload_model_from_bucket(blob_name,file_path,bucket_name):
+    try:
+        bucket = storage_client.get_bucket(bucket_name)
+        blob = bucket.blob(blob_name)
+        with open(file_path, 'wb') as f:
+            storage_client.download_blob_to_file(blob, f)
+        return True
+    except Exception as e:
+        print(e)
+        return False
+
+donwload_model_from_bucket('bert_model/config.json', os.path.join('bert_model/','config.json'),'qwiklabs-gcp-02-96ccf592dc5f-test-bucket')
+donwload_model_from_bucket('bert_model/tf_model.h5', os.path.join('bert_model/','tf_model.h5'),'qwiklabs-gcp-02-96ccf592dc5f-test-bucket')
+
+# Load from our deployed model in Huggingface.co
+tokenizer = AutoTokenizer.from_pretrained(
+    "Andaf/bert-uncased-finetuned-squad-indonesian")
+
+model = TFAutoModelForQuestionAnswering.from_pretrained(
+        './bert_model/',return_dict=False)
+
+nlp = pipeline('question-answering', model=model, tokenizer=tokenizer)
+
+def predict_func(question):
+
+    context = "Makanan kesukaan saya adalah rendang."
+
+    result = nlp(
+        question=question,
+        context=context
+    )
+
+    answer = result['answer']
+    confidence = result['score']
+    return answer, confidence
