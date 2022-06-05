6074c251cded82b49fe1af93bb4f878a1c8fde7e
Aclaputra <muhamadacla22@gmail.com>
2022-06-05 13:58:01 +0700

    cleaning & migrating files

--- CC/terraform/k8s/Deploy GKE Cluster/provider.tf
+++
@@ -1,8 +0,0 @@
-provider "google" {
-    # Make service account and CREATE JSON key
-  credentials = file("./.json") #directory file diisi ama letak JSON key
-  project     = "PROJECT-ID"
-  region      = "us-central1"
-  version     = "~> 2.5.0"
-}
-
